acc97e2b016500901dfe319470679e398091b70e
Hernán Valdivieso <hfvaldivieso@uc.cl>
2023-04-12 14:35:12 -0400

    fix opacity and style text

--- grafo.js
+++ grafo.js
@@ -1,18 +1,18 @@
 data_path = "Datos/specific_new/333.json"
 function runCode() {
     d3.json(data_path)
-    .then((data) => {
-        // data.comments = comments_processed(data.comments);
-        createGrafo(data);
-      })
-      .catch((error) => console.log(error));
+        .then((data) => {
+            // data.comments = comments_processed(data.comments);
+            createGrafo(data);
+        })
+        .catch((error) => console.log(error));
 }
 
 runCode();
 
 // -------------------------------- Limpiar Datos ----------------------
 
-function data_processed(d){
+function data_processed(d) {
     let data = {
         title: d.title,
         creator: d.agency,
@@ -28,14 +28,14 @@ function data_processed(d){
     return data
 }
 
-function create_tree_comments(comments){
+function create_tree_comments(comments) {
     new_comments = [];
-    for (comment of comments){
-        if (comment.level == 0){
+    for (comment of comments) {
+        if (comment.level == 0) {
             new_comments.push(comment_processed(comment));
         } else {
-            for (father of new_comments){
-                if (father.id == comment.parentId){
+            for (father of new_comments) {
+                if (father.id == comment.parentId) {
                     father.comments.push(comment_processed(comment));
                     break;
                 }
@@ -45,7 +45,7 @@ function create_tree_comments(comments){
     return new_comments
 }
 
-function comment_processed(c){
+function comment_processed(c) {
     let comment = {
         id: c.id,
         creator: c.creator,
@@ -57,10 +57,10 @@ function comment_processed(c){
     return comment
 }
 
-function max_level(comments){
+function max_level(comments) {
     max = 0;
-    for (comment of comments){
-        if (max < comment.level){
+    for (comment of comments) {
+        if (max < comment.level) {
             max = comment.level;
         }
     }
@@ -73,90 +73,94 @@ function max_level(comments){
 const HEIGTH = 40;
 const WIDTH = 400;
 
-function createGrafo(data){
+function createGrafo(data) {
 
-// Constantes
+    // Constantes
     const tree_depth = max_level(data.comments);
     const tree_height = data.comments.length;
 
-    const margin = {top: 20, right: 150, bottom: 30, left: 90},
-        width  = WIDTH * Math.sqrt(tree_depth + 1) - margin.left - margin.right,
-        height = (100 + HEIGTH * tree_height) - margin.top - margin.bottom;
+    const margin = { top: 20, right: 150, bottom: 30, left: 90 };
+    const width = WIDTH * Math.sqrt(tree_depth + 1) - margin.left - margin.right;
+    const height = (100 + HEIGTH * tree_height) - margin.top - margin.bottom;
 
-// Modificar Datos
+    // Modificar Datos
     data = data_processed(data);
     const COLOR = d3.scaleOrdinal(d3[`schemeTableau10`])
-    .domain([...Array(tree_depth).keys()]);
-    data.comments = create_tree_comments(data.comments);
-    const treemap = d3.tree().size([height, width]);
+        .domain([...Array(tree_depth).keys()]);
 
-// Crear Grafo
+    data.comments = create_tree_comments(data.comments);
+
+    // Crear Grafo
     let nodes = d3.hierarchy(data, d => d.comments);
 
+    const treemap = d3.tree().size([height, width]);
     nodes = treemap(nodes);
-    const svg = d3.select("#vis-1").append("svg")
+    const svg = d3.select("#vis-1")
+        .append("svg")
         .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom),
-      g = svg.append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+        .attr("height", height + margin.top + margin.bottom)
 
-    var linkGen = d3.linkHorizontal()
+    const g = svg.append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    let linkGen = d3.linkHorizontal()
         .source(d => [d.y, d.x])
         .target(d => [d.parent.y, d.parent.x]);;
 
 
     const link = g.selectAll(".link")
-    .data( nodes.descendants().slice(1))
-  .enter().append("path")
-    .attr("class", "link")
-    .style("stroke", d => COLOR(d.data.level))
-    .attr("d", linkGen);
+        .data(nodes.descendants().slice(1))
+        .enter().append("path")
+        .attr("class", "link")
+        .style("stroke", d => COLOR(d.data.level))
+        .attr("d", linkGen);
 
-       const node = g.selectAll(".node")
-    .data(nodes.descendants())
-    .enter().append("g")
-    .attr("class", d => "node" + (d.comments ? " node--internal" : " node--leaf"))
-    .attr("transform", d => "translate(" + d.y + "," + d.x + ")");
+    const node = g.selectAll(".node")
+        .data(nodes.descendants())
+        .enter().append("g")
+        .attr("class", d => "node" + (d.comments ? " node--internal" : " node--leaf"))
+        .attr("transform", d => `translate(${d.y}, ${d.x})`);
 
     radius = d3.scaleSqrt()
-  .domain([0, 10])
-  .range([5, 20])
+        .domain([0, 10])
+        .range([5, 20])
 
     node.append("circle")
-  .attr("r", d => radius(d.data.likes))
-  .style("stroke", d => d.data.likes)
-  .style("fill", d => COLOR(d.data.level));
+        .attr("r", d => radius(d.data.likes))
+        .style("stroke", d => d.data.likes)
+        .style("fill", d => COLOR(d.data.level));
 
-  node.append("text")
-  .attr("dy", ".35em")
-  .attr("x", d =>  radius(d.data.likes) + 5)
-  .attr("y", d => 20)
-  .style("text-anchor", d => d.comments ? "end" : "start")
-  .text(d => d.data.creator);
+    node.append("text")
+        .attr("dy", ".35em")
+        .attr("x", d => radius(d.data.likes) + 5)
+        .attr("y", d => 20)
+        .style("text-anchor", d => d.comments ? "end" : "start")
+        .text(d => d.data.creator);
 
 
-  // Filtro Fechas
-    function filtrar_fecha(timeMin, timeMax, time){
-        console.log(timeMin);
-        console.log(timeMax);
-        console.log(time);
-        if ((time > timeMin) && (time < timeMax)){
-            console.log("epic")
-            return 1;
-        } else {
-            return 0.5;
-        }
+    // Filtro Fechas
+    function filtrar_fecha(timeMin, timeMax, time) {
+        // console.log("MIN", timeMin);
+        // console.log("MAX", timeMax);
+        // console.log("time", time);
+        // console.log()
+        if (time < timeMin) { return 0.4 }
+        if (time > timeMax) { return 0.1 }
+        return 1;
     }
 
-  d3.select("#selectButton").on("click", function(d) {
-      var timeMin = document.getElementById('date_min').value;
-      timeMin = Date.parse(timeMin)
-      var timeMax = document.getElementById('date_max').value;
-      timeMax = Date.parse(timeMax)
-      console.log(timeMin);
-      console.log(timeMax);
-      node.select("circle")
-      .attr("opacity", d => filtrar_fecha(timeMin, timeMax, d.data.time))
-  })
+    d3.select("#selectButton").on("click", function (d) {
+        let timeMin = document.getElementById('date_min').value;
+        let timeMax = document.getElementById('date_max').value;
+
+        timeMin = Date.parse(timeMin)
+        timeMax = Date.parse(timeMax)
+
+        node.selectAll("circle")
+            .attr("opacity", d => filtrar_fecha(timeMin, timeMax, d.data.time))
+
+        link.attr("opacity", d => {
+            return filtrar_fecha(timeMin, timeMax, d.data.time)
+        })
+    })
 }
