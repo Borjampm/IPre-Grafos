0fd41b22ec286589ced2c858cec5c95ad759d59f
Hernán Valdivieso <hfvaldivieso@uc.cl>
2023-06-07 14:26:12 -0400

    exec linter

--- grafo.js
+++ grafo.js
@@ -8,7 +8,7 @@ function runCode(i) {
 }
 
 // Crear selector
-const PRIMERANOTICIA = 61; // Parametro para elegir la primera noticia que se muestra
+const PRIMERANOTICIA = 1; // Parametro para elegir la primera noticia que se muestra
 const SELECTOR = d3.select("#selector").append("select").attr("id", "selectorObject");
 
 SELECTOR.selectAll("option")
@@ -85,31 +85,32 @@ function max_level(comments) {
 // Parametros
 const SVG = d3.select("#vis-1").append("svg");
 SVG.append("g")
+
 let Tooltip = d3.select("#vis-1")
-.append("div")
-.style("opacity", 0)
-.attr("class", "tooltip")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
 
 const ZOOM = d3.zoom()
     .on("zoom", handleZoom);
 function handleZoom(e) {
     d3.select("svg g")
-    .attr("transform", e.transform);
+        .attr("transform", e.transform);
 }
 const resetZOOM = d3.zoom()
     .on("zoom", resetZoom);
 function resetZoom(e) {
     d3.select("svg g")
         .attr("transform", "translate(0,0) scale(1)");
-    }
+}
 
 runCode(PRIMERANOTICIA);
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
 
-  // ---------------------------------------------- Filtrar datos en intervalos -----------------------------------
+// ---------------------------------------------- Filtrar datos en intervalos -----------------------------------
 async function dataInterval(unfiltered_data) {
     const SEGUNDOS = 5;
     const targetLength = SEGUNDOS * 1000;   // Tiempo que se quiere que dure la animacion
@@ -130,7 +131,7 @@ async function dataInterval(unfiltered_data) {
         let comments = data.comments;
         comments.sort((a, b) => a.time - b.time);
 
-        let min = Date.parse(timePublish+":00.000+00:00");
+        let min = Date.parse(timePublish + ":00.000+00:00");
         let max = Date.parse(last_comment_time);
         let timePerNode = targetLength / (max - min);
 
@@ -174,12 +175,12 @@ function createHistogram(unfiltered_data) {
     let data = data_processed(unfiltered_data);
     let timePublish = transform_min_date(data.date, data.time);
     let last_comment_time = get_last_comment_time(data.comments);
-    let minDate = Date.parse(timePublish+":00.000+00:00");
+    let minDate = Date.parse(timePublish + ":00.000+00:00");
     let maxDate = Date.parse(last_comment_time);
-        // set the dimensions and margins of the graph
-    const margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    // set the dimensions and margins of the graph
+    const margin = { top: 10, right: 30, bottom: 30, left: 40 },
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
     const SVG2 = d3.select("#vis-2")
@@ -187,22 +188,23 @@ function createHistogram(unfiltered_data) {
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
-        .attr("transform",
-            `translate(${margin.left},${margin.top})`);
-        histograma(data.comments, minDate, maxDate)
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    histograma(data.comments, minDate, maxDate)
 
     function histograma(data, minDate, maxDate) {
 
         const x = d3.scaleLinear()
             .domain([minDate, maxDate])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
             .range([0, width]);
+
         SVG2.append("g")
             .attr("transform", `translate(0, ${height})`)
             .call(d3.axisBottom(x));
 
         // set the parameters for the histogram
         const histogram = d3.histogram()
-            .value(function(d) { console.log(d); return d.time; })   // I need to give the vector of value
+            .value(function (d) { console.log(d); return d.time; })   // I need to give the vector of value
             .domain(x.domain())  // then the domain of the graphic
             .thresholds(x.ticks(20)); // then the numbers of bins
 
@@ -211,22 +213,22 @@ function createHistogram(unfiltered_data) {
 
         // Y axis: scale and draw:
         const y = d3.scaleLinear()
-        .range([height, 0]);
-        y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+            .range([height, 0]);
+        y.domain([0, d3.max(bins, function (d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
         SVG2.append("g")
-        .call(d3.axisLeft(y));
+            .call(d3.axisLeft(y));
 
         // append the bar rectangles to the svg element
         SVG2.selectAll("rect")
-        .data(bins)
-        .join("rect")
+            .data(bins)
+            .join("rect")
             .attr("x", 1)
-        .attr("transform", function(d) { return `translate(${x(d.x0)} , ${y(d.length)})`})
-            .attr("width", function(d) { return x(d.x1) - x(d.x0)})
-            .attr("height", function(d) { return height - y(d.length); })
+            .attr("transform", function (d) { return `translate(${x(d.x0)} , ${y(d.length)})` })
+            .attr("width", function (d) { return x(d.x1) - x(d.x0) })
+            .attr("height", function (d) { return height - y(d.length); })
             .style("fill", "#69b3a2")
 
-        };
+    };
 
 }
 
@@ -238,7 +240,7 @@ function createGrafo(unfiltered_data, data, time_sleep) {
     const circleRadius = 15;
 
     // Tiempo que toma actualizar el grafo
-    const updateDurationTime = time_sleep/3
+    const updateDurationTime = time_sleep / 3
     // Tiempo que toma agregar nuevos elementos
     const enterDurationTime = time_sleep - updateDurationTime;
 
@@ -248,7 +250,7 @@ function createGrafo(unfiltered_data, data, time_sleep) {
     const margin = { top: 20, right: 30, bottom: 30, left: 90 };
     // Ajustar el ancho para que mínimo sea de 300 pixeles
     const width = Math.max(circleRadius * tree_height * 2.1, WIDTH);
-    const height = Math.max(full_depth * 500 , HEIGTH);
+    const height = Math.max(full_depth * 500, HEIGTH);
     // const height = 1000;
 
     const colorScale = d3.scaleDiverging(d => d3.interpolateRdYlBu(d))
@@ -280,7 +282,7 @@ function createGrafo(unfiltered_data, data, time_sleep) {
         .join(enter => {
             const dato_nuevo = enter.append("path")
                 .attr("class", "link")
-                .style("stroke", d => colorScale((d.data.dislikes + d.data.likes) == 0 ? 0.5 : d.data.likes/(d.data.likes + d.data.dislikes)))
+                .style("stroke", d => colorScale((d.data.dislikes + d.data.likes) == 0 ? 0.5 : d.data.likes / (d.data.likes + d.data.dislikes)))
                 .attr("d", linkGenInitial)
 
             // Antes de aparecer uno nuevo, espero que se actualice lo anterior
@@ -311,33 +313,33 @@ function createGrafo(unfiltered_data, data, time_sleep) {
                 .attr("y_original", d => d.y);
 
             node_nuevo.append("circle")
-                .attr("class", d => {return d.parent?'comentario':'titulo'})
+                .attr("class", d => { return d.parent ? 'comentario' : 'titulo' })
                 .attr("r", 0)
                 .style("stroke", d => d.data.likes)
-                .style("fill", d => colorScale((d.data.dislikes + d.data.likes) == 0 ? 0.5 : d.data.likes/(d.data.likes + d.data.dislikes)));
+                .style("fill", d => colorScale((d.data.dislikes + d.data.likes) == 0 ? 0.5 : d.data.likes / (d.data.likes + d.data.dislikes)));
 
-                // Antes de aparecer uno nuevo, espero que se actualice lo anterior
-                node_nuevo.transition()
-                    .delay(updateDurationTime)
-                    .duration(enterDurationTime)
-                    .attr("transform", d => `translate(${d.x}, ${d.y})`);
+            // Antes de aparecer uno nuevo, espero que se actualice lo anterior
+            node_nuevo.transition()
+                .delay(updateDurationTime)
+                .duration(enterDurationTime)
+                .attr("transform", d => `translate(${d.x}, ${d.y})`);
 
-                node_nuevo.select("circle")
-                    .transition()
-                    .delay(updateDurationTime)
-                    .duration(enterDurationTime)
-                    .attr("r", circleRadius);
+            node_nuevo.select("circle")
+                .transition()
+                .delay(updateDurationTime)
+                .duration(enterDurationTime)
+                .attr("r", circleRadius);
 
-                return node_nuevo
-            }, update => {
-                update.transition()
-                    .duration(updateDurationTime)
-                    .attr("transform", d => `translate(${d.x}, ${d.y})`);
+            return node_nuevo
+        }, update => {
+            update.transition()
+                .duration(updateDurationTime)
+                .attr("transform", d => `translate(${d.x}, ${d.y})`);
 
             return update
         }, exit => {
             exit.remove()
-    })
+        })
 
     node.raise();
 
@@ -377,7 +379,7 @@ function createGrafo(unfiltered_data, data, time_sleep) {
 
 
     // ---------------------------------------------- Tooltip ----------------------------------------------
-        node.selectAll(".titulo")
+    d3.selectAll(".titulo")
         .on("mouseleave", function (event, d) {
             Tooltip.style("opacity", 0)
                 .style("display", "none")
@@ -401,7 +403,7 @@ function createGrafo(unfiltered_data, data, time_sleep) {
                 .style("top", (event.pageY + -10) + "px")
         })
 
-        node.selectAll(".comentario")
+    node.selectAll(".comentario")
         .on("mouseleave", function (event, d) {
             Tooltip.style("opacity", 0)
                 .style("display", "none")
