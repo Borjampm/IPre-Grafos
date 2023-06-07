91567b46f6772c07d5b5c1809f4bfcc0548f811b
Borjampm <bmarquezdelaplata@uc.cl>
2023-06-06 20:19:46 -0400

    empezando histograma

--- grafo.js
+++ grafo.js
@@ -85,6 +85,8 @@ function max_level(comments) {
 // Parametros
 const SVG = d3.select("#vis-1").append("svg");
 SVG.append("g")
+const SVG2 = d3.select("#vis-2").append("svg");
+SVG.append("g")
 let Tooltip = d3.select("#vis-1")
 .append("div")
 .style("opacity", 0)
@@ -125,7 +127,6 @@ async function dataInterval(unfiltered_data) {
     } else {
         let timePublish = transform_min_date(data.date, data.time);
         let last_comment_time = get_last_comment_time(data.comments);
-        console.log(timePublish, last_comment_time);
         document.getElementById('status').innerText = 'Generando grafo... (No cambiar de noticia)';
 
         let comments = data.comments;
@@ -143,11 +144,11 @@ async function dataInterval(unfiltered_data) {
         data.comments = aux;
         data.comments = create_tree_comments(data.comments);
         createGrafo(unfiltered_data, data, time_sleep);
+        createHistogram(unfiltered_data)
         for (let comment of comments) {
             if (aux.length == 0) {
                 // time_sleep = timePerNode * (comment.time - min);
                 time_sleep = timePerNode;
-                console.log(time_sleep)
                 aux.push(comment);
                 data.comments = aux;
                 data.comments = create_tree_comments(data.comments);
@@ -156,7 +157,6 @@ async function dataInterval(unfiltered_data) {
             } else {
                 // time_sleep = timePerNode * (comment.time - aux[aux.length - 1].time);
                 time_sleep = timePerNode;
-                console.log(time_sleep)
                 aux.push(comment);
                 data.comments = aux;
                 data.comments = create_tree_comments(data.comments);
@@ -168,6 +168,57 @@ async function dataInterval(unfiltered_data) {
         d3.select("svg")
             .call(ZOOM);
     }
+}
+
+// ---------------------------------------------- Crear Histograma -----------------------------------
+function createHistogram(unfiltered_data) {
+    // set the dimensions and margins of the graph
+    let margin = {top: 10, right: 30, bottom: 30, left: 40},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    let data = data_processed(unfiltered_data);
+    let timePublish = transform_min_date(data.date, data.time);
+    let last_comment_time = get_last_comment_time(data.comments);
+    let minDate = Date.parse(timePublish+":00.000+00:00");
+    let maxDate = Date.parse(last_comment_time);
+    console.log(minDate, maxDate);
+
+    // X axis: scale and draw:
+    let x = d3.scaleLinear()
+        .domain([minDate, maxDate])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+        .range([0, width]);
+    SVG2.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // set the parameters for the histogram
+    let histogram = d3.histogram()
+        .value(function(d) { return 1; })   // I need to give the vector of value
+        .domain(x.domain())  // then the domain of the graphic
+        .thresholds(x.ticks(10)); // then the numbers of bins
+
+    // And apply this function to data to get the bins
+    let bins = histogram(data);
+
+    // Y axis: scale and draw:
+    let y = d3.scaleLinear()
+        .range([height, 0]);
+        y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+    SVG2.append("g")
+        .call(d3.axisLeft(y));
+
+    // // append the bar rectangles to the svg element
+    SVG2.selectAll("rect")
+        .data(bins)
+        .enter()
+        .append("rect")
+            .attr("x", 1)
+            .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+            .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+            .attr("height", function(d) { return height - y(d.length); })
+            .style("fill", "#69b3a2")
+
 }
 
 // ---------------------------------------------- Crear Grafo -----------------------------------
@@ -304,7 +355,6 @@ function createGrafo(unfiltered_data, data, time_sleep) {
         timeMin = Date.parse(timeMin);
         timeInterval = timeInterval * 60 * 1000;
         timeMax = timeMin + timeInterval;
-        console.log(timeMin, timeMax)
 
         node.selectAll("circle")
             .attr("opacity", d => {
